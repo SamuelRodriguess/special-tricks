39ccf4db62d42858e91c895f925b20fc585058cb
samuel.rodrigues <samuel.rodrigues@acct>
2023-06-06 19:01:58 -0300

    feat: memoize function

---
+++ react/utils/memoization.ts
@@ -0,0 +1,23 @@
+function expensiveFunction(arg: number) {
+  return arg * 2
+}
+
+function memoizeFunction(fn: (value: number) => number) {
+  const cache = new Map()
+
+  return function (arg: number) {
+    if (cache.has(arg)) {
+      return cache.get(arg)
+    }
+    const result = fn(arg)
+
+    cache.set(arg, result)
+
+    return result
+  }
+}
+
+const memoizedExpensiveFunction = memoizeFunction(expensiveFunction);
+
+console.log(memoizedExpensiveFunction(2)); // Output: Calculating result... 4
+console.log(memoizedExpensiveFunction(2)); // Output: Returning cached result... 4
