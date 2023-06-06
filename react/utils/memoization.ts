1f72f8998a921942f99cb60cd1f434f9b5a2b583
samuel.rodrigues <samuel.rodrigues@acct>
2023-06-06 20:30:53 -0300

    chore: description  memoize

--- react/utils/memoization.ts
+++ react/utils/memoization.ts
@@ -4,6 +4,11 @@ function expensiveFunction(arg: number) {
 
 // esse conceito é usado no React com o hook useMemo
 
+// é importante destacar que o useMemo e o memoize têm finalidades diferentes.
+// O useMemo é específico para componentes React e é útil quando você deseja memoizar
+// valores dentro de um componente de função. O memoize, por outro lado, é uma função genérica
+// que pode ser usada para  memoizar qualquer função em JavaScript, independentemente do contexto do React.
+
 function memoizeFunction(fn: (value: number) => number) {
   const cache = new Map()
 
