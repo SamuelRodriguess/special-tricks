1c2705e5f57ed2a07ebe1473aea546184e5c4e83
samuel.rodrigues <samuel.rodrigues@acct>
2023-06-06 20:24:41 -0300

    fix: eslint disable

--- react/utils/curryingMemoization.ts
+++ react/utils/curryingMemoization.ts
@@ -1,21 +1,30 @@
+/* eslint-disable func-names */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
 function expensiveFunction(arg1, arg2, arg3) {
-  console.log('Calculating result...');
-  return arg1 + arg2 + arg3;
-}
+  console.log('Calculating result...')
 
+  return arg1 + arg2 + arg3
+}
 
 // utils/memoization
 function memoize(fn) {
-  const cache = new Map();
+  const cache = new Map()
+
   return function memoized(...args) {
-    const key = args.join('-');
+    const key = args.join('-')
+
     if (cache.has(key)) {
-      console.log('Returning cached result...');
-      return cache.get(key);
+      console.log('Returning cached result...')
+
+      return cache.get(key)
     }
-    const result = fn(...args);
-    cache.set(key, result);
-    return result;
+
+    const result = fn(...args)
+
+    cache.set(key, result)
+
+    return result
   }
 }
 
@@ -23,17 +32,17 @@ function memoize(fn) {
 function curry(fn) {
   return function curried(...args) {
     if (args.length >= fn.length) {
-      return fn(...args);
-    } else {
-      return function(...moreArgs) {
-        return curried(...args, ...moreArgs);
-      }
+      return fn(...args)
+    }
+
+    return function (...moreArgs) {
+      return curried(...args, ...moreArgs)
     }
   }
 }
 
-const memoizedCurriedFunction = memoize(curry(expensiveFunction));
+const memoizedCurriedFunction = memoize(curry(expensiveFunction))
 
-console.log(memoizedCurriedFunction(1)(2)(3)); // Output: Calculating result... 6
+console.log(memoizedCurriedFunction(1)(2)(3)) // Output: Calculating result... 6
 // se ja fez o calculo, a função apenas retorna o valor novamente!!!!!!
-console.log(memoizedCurriedFunction(1)(2)(3)); // Output: Returning cached result... 6
+console.log(memoizedCurriedFunction(1)(2)(3)) // Output: Returning cached result... 6
